146fc7f46ff5ef636623750e780544904be89857
nowrin <nowrinnova0088@gmail.com>
2024-02-16 00:17:22 +0600

    add

--- Task Manager App/src/component/Header.jsx
+++ Task Manager App/src/component/Header.jsx
@@ -3,12 +3,12 @@ const Container = () => {
   return (
     <>
       <div className={styles.container}>
-        <span className={styles.dot1}>dot</span>
-        <span className={styles.dot2}>dot</span>
-        <span className={styles.dot3}>dot</span>
-        <span className={styles.menu_bar}>
+        <span className={styles.dot1}></span>
+        <span className={styles.dot2}></span>
+        <span className={styles.dot3}></span>
+        <div class={styles.menu_bar}>
           <i class="fa-solid fa-bars"></i>
-        </span>
+        </div>
       </div>
     </>
   );
