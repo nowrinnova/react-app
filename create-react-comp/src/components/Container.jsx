f65abcea3b7ff739797607bf66d57078185e2a27
nowrin <nowrinnova0088@gmail.com>
2024-02-15 20:41:59 +0600

    add

--- create-react-comp/src/components/Container.jsx
+++ create-react-comp/src/components/Container.jsx
@@ -1,5 +1,5 @@
-import styles from "./Container.module.css"
-const Container=({children})=>{
- return <div className={styles.container}>{children}</div>
-}
+import styles from "./Container.module.css";
+const Container = ({ children }) => {
+  return <div className={styles.container}>{children}</div>;
+};
 export default Container;
